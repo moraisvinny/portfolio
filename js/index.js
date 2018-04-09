6164641e3862fe5492c2fa71bbe4b6bc3bde24a4
Vinicius Morais <vinicius.reis.morais@gmail.com>
2018-04-09 16:20:42 -0300

    modal e imagens

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('#botaoModal').click(function() {
+    
+    $('.modal-title').text('AAAAA');
+    $('.modal-body').text('TESTE ALTERANDO');
+});
+
+// $('#piccoli').click(function() {
+//     $('.modal-title').text('PICCOLI VESTITI');
+//     $('.modal-body').html('');
+// });
